6435d426ee8d9d55013c1a351d528c3719308918
akash <akashpayne2@gmail.com>
2020-10-16 01:36:22 +0100

    Added body parser to server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 import express from "express";
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.get("/", (req, res) => {
-    res.send("hello world");
-});
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// db
+
+// app
+
 app.listen(PORT, () => {
     console.log(`Server up and running on port ${PORT}`);
 });
