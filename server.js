4c7ac17255260f4ee7e0c5c1934f2002418b5fe1
akash <akashpayne2@gmail.com>
2020-10-16 01:36:44 +0100

    Updated server for database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-import express from "express";
+const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+require("dotenv").config(); // for loading environment variables
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -9,8 +11,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // db
-
+const MONGO_URI = process.env.MONGO_URI;
+mongoose
+    .connect(MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.info("Mongo Connection successful"))
+    .catch(err => console.error("err", err));
 // app
+
 
 app.listen(PORT, () => {
     console.log(`Server up and running on port ${PORT}`);
